79572aa3c18be14e849ee3d1e4175ce1589d3d99
Crowdbotics <team@crowdbotics.com>
2022-12-30 17:48:15 +0000

    Add

--- screens/index.js
+++ screens/index.js
@@ -1,99 +1,154 @@
-import React from "react";
-import { Text, StyleSheet, View, Image, Pressable } from "react-native";
+import * as React from "react";
+import { Text, View, StyleSheet, TextInput, TouchableHighlight } from "react-native";
 
-const UploadMediaScreen = (params) => {
+const pressed = () => {
+  console.log("pressed");
+};
+
+const ForgotPassword = () => {
   return (
     <View style={styles.container}>
-      <View style={styles.menu}>
-        <View style={styles.menuItem}>
-          <Text style={styles.menuItemText}>Take photo or video</Text>
-          <Image source={require("./assets/cameraIcon.png")} />
-        </View>
-        <View style={styles.menuItem}>
-          <Text style={styles.menuItemText}>Photo Library</Text>
-          <Image source={require("./assets/libraryIcon.png")} />
-        </View>
-        <View style={styles.menuItem}>
-          <Text style={styles.menuItemText}>Browse</Text>
-          <Image source={require("./assets/menuIcon.png")} />
-        </View>
+      <View style={styles.topHead}>
+        <Text style={styles.mainHeading}>Forgot {"\n"} password</Text>
       </View>
-      <Button buttonText="Cancel" outline={true} />
+
+      <View style={styles.inputSection}>
+      <View style={styles.newPassword}>
+        <Text style={styles.newpasswordText}>Set new password for your account.</Text>
+      </View>
+      <View style={styles.passwordInput}>
+        <Text style={styles.newpasswordLabel}>Password</Text>
+        <Input placeholder="Enter"></Input>
+      </View>
+      <View style={styles.confirmInput}>
+        <Text style={styles.newpasswordLabel}>Confirm Password</Text>
+        <Input placeholder="Enter"></Input>
+      </View>
+      </View>
+      <View style={styles.resetButton}>
+        <Button onPress={pressed} style={styles.resetBtn}>Reset password</Button>
+      </View>
+      <View style={styles.back}>
+        <Text style={styles.backText}>Back</Text>
+      </View>
     </View>
   );
 };
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    padding: 20,
-    justifyContent: "flex-end"
-  },
-  menuItem: {
+  topHead: {
+    display: "flex",
     flexDirection: "row",
     alignItems: "center",
-    paddingHorizontal: 20,
-    paddingVertical: 15,
-    justifyContent: "space-between",
-    borderBottomWidth: 1,
-    borderColor: "#ccc"
+    justifyContent: "center",
+    textAlign: "center"
   },
-  menuItemText: {
-    fontSize: 18,
+  mainHeading: {
+    fontSize: 30,
     fontWeight: "bold",
-    color: "#000",
-    textAlign: "left"
+    textAlign: "center"
+  },
+  container: {
+    padding: 20,
+    backgroundColor: "#FFF",
+    height: "100%"
+  },
+  inputSection: {
+    paddingTop: 40
+  },
+  newPassword: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center"
+
+  },
+  passwordInput: {
+    marginTop: 50
+  },
+  newpasswordLabel: {
+    paddingLeft: 15,
+    paddingBottom: 7
+  },
+  confirmInput: {
+    paddingTop: 10
+  },
+  resetButton: {
+    paddingTop: 20,
+    paddingLeft: 30,
+    paddingRight: 30
+  },
+  back: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: 30
+  },
+  backText: {
+    fontWeight: "600",
+    fontSize: 20
   }
 });
-export default UploadMediaScreen;
 
-const Button = (params) => {
-  const btnStyle = {
-    backgroundColor: params.outline ? "#fff" : "#000",
-    borderColor: params.outline ? "#000" : "#fff",
-    borderWidth: 1
-  };
-  const btnText = {
-    color: params.outline ? "#000" : "#fff"
-  };
+export default ForgotPassword;
+
+const Button = (props) => {
   return (
-    <View style={buttonStyles.btnContainer}>
-      <Pressable style={[buttonStyles.btn, btnStyle]} onPress={params.onPress}>
-        <Text style={[buttonStyles.btnText, btnText]}>{params.buttonText}</Text>
-        <View style={styles.childrenContainer}>{params.children}</View>
-      </Pressable>
+    <TouchableHighlight onPress={props.onPress} underlayColor='#DDDDDD'>
+      <View style={[btnStyles.button, {
+        backgroundColor: props.backgroundColor ? props.backgroundColor : "#000000",
+        height: props.height ? props.height : 49,
+        borderWidth: props.borderWidth ? props.borderWidth : 0,
+        borderColor: props.borderColor ? props.borderColor : "#000000"
+      }]}>
+        <Text style={[btnStyles.text, { color: props.color ? props.color : "#ffffff" }]}>{props.children}</Text>
+      </View>
+    </TouchableHighlight>
+  );
+};
+
+const btnStyles = StyleSheet.create({
+  button: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10
+  },
+  text: {
+    fontWeight: "bold",
+    fontSize: 15
+  }
+});
+
+const Input = (props) => {
+  return (
+    <View>
+      <TextInput
+        style={inputStyles.input}
+        placeholder={props.placeholder}
+        value={props.value}
+        onChangeText={(num) => props.setValue(num)}
+        placeholderTextColor='#ddd'
+        editable={props.editable !== false}
+      />
+      {props.errorText ? <Text style={inputStyles.error}>{props.errorText}</Text> : null}
     </View>
   );
 };
 
-const buttonStyles = StyleSheet.create({
-  btnContainer: {
-    paddingTop: 10,
-    paddingHorizontal: 20,
-    justifyContent: "center",
-    marginTop: 20
-  },
-  btn: {
-    backgroundColor: "black",
-    height: 50,
-    width: "100%",
-    padding: 10,
-    paddingHorizontal: 25,
+const inputStyles = StyleSheet.create({
+  input: {
+    backgroundColor: "#fff",
+    height: 53,
+    borderColor: "#C4C4C4",
+    color: "#000",
     borderRadius: 10,
-    justifyContent: "center",
-    alignItems: "center",
-    shadowColor: "rgba(0, 0, 0, 0.2)",
-    elevation: 10,
-    flexDirection: "row"
+    fontSize: 14,
+    borderWidth: 1,
+    paddingHorizontal: 15
   },
-  btnText: {
-    color: "#fff",
-    fontSize: 16,
-    fontWeight: "bold"
-  },
-  childrenContainer: {
-    justifyContent: "center",
-    alignItems: "center"
+  error: {
+    fontSize: 13,
+    color: "#FA060D",
+    paddingTop: 8
   }
 });
