31329db7df7d6437b8b5335b256d75dae3b4742b
Crowdbotics <team@crowdbotics.com>
2022-12-30 17:47:38 +0000

    Add

--- screens/index.js
+++ screens/index.js
@@ -1,72 +1,56 @@
 import React from "react";
-import {
-  Text,
-  View,
-  StyleSheet, Image, ScrollView, Pressable
-} from "react-native";
+import { Text, StyleSheet, View, Image, Pressable } from "react-native";
 
-const SubscriptionDetails = () => {
+const UploadMediaScreen = (params) => {
   return (
-    <ScrollView style={styles.container}>
-      <View style={styles.header}>
-        <Image source={require("./assets/back.png")} style={styles.back} />
-        <Text style={styles.heading}>Subscription</Text>
-        <Text />
-      </View>
-      <View style={styles.reviewContainer}>
-        <Text style={styles.review}>Current Subscription</Text>
-        <Text style={styles.edit}>Free</Text>
-      </View>
-
-      <View style={styles.listContainer}>
-        <Text style={styles.title}>Premium Subscription</Text>
-        <View style={styles.descContainer}>
-          <Text style={styles.desc}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas ultrices risus at neque, at. Cras cursus parturient nec, quam convallis viverra arcu. Lorem pellentesque cras vulputate velit ante tellus.
-          </Text>
-          <Text style={styles.desc}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas ultrices risus at neque, at. Cras cursus parturient nec, quam convallis viverra arcu. Lorem pellentesque cras vulputate velit ante tellus.
-          </Text>
+    <View style={styles.container}>
+      <View style={styles.menu}>
+        <View style={styles.menuItem}>
+          <Text style={styles.menuItemText}>Take photo or video</Text>
+          <Image source={require("./assets/cameraIcon.png")} />
         </View>
-        <View style={styles.priceContainer}>
-          <Text style={styles.price}>Price:</Text>
-          <Text style={styles.price}> $14.99</Text>
+        <View style={styles.menuItem}>
+          <Text style={styles.menuItemText}>Photo Library</Text>
+          <Image source={require("./assets/libraryIcon.png")} />
         </View>
-
-        <Button buttonText={"Upgrade to Premium"} />
+        <View style={styles.menuItem}>
+          <Text style={styles.menuItemText}>Browse</Text>
+          <Image source={require("./assets/menuIcon.png")} />
+        </View>
       </View>
-
-    </ScrollView>
+      <Button buttonText="Cancel" outline={true} />
+    </View>
   );
 };
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+    backgroundColor: "#fff",
+    padding: 20,
+    justifyContent: "flex-end"
   },
-  header: {
+  menuItem: {
     flexDirection: "row",
-    justifyContent: "space-between",
     alignItems: "center",
-    marginHorizontal: 30,
-    marginTop: 15,
-    marginBottom: 30
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+    justifyContent: "space-between",
+    borderBottomWidth: 1,
+    borderColor: "#ccc"
   },
-  back: { width: 11.25, height: 20, resizeMode: "contain", marginLeft: -15 },
-  heading: { fontSize: 16, color: "#000" },
-  reviewContainer: { flexDirection: "row", justifyContent: "space-between", alignItems: "center", backgroundColor: "#fff", paddingHorizontal: 20, paddingVertical: 20, marginVertical: 15 },
-  review: { fontSize: 16, color: "#231F20" },
-  edit: { color: "#12D790", fontSize: 16 },
-  listContainer: { flex: 1, backgroundColor: "#FFF", marginTop: 30, paddingHorizontal: 10, paddingBottom: 25 },
-  title: { fontSize: 24, marginLeft: 20, marginTop: 15, marginBottom: 20, fontWeight: "bold" },
-  descContainer: { paddingHorizontal: 20 },
-  desc: { color: "#757575", marginBottom: 5 },
-  priceContainer: { flexDirection: "row", justifyContent: "space-between", alignItems: "center", paddingHorizontal: 20, marginVertical: 20 },
-  price: { fontSize: 24, fontWeight: "bold" }
+  menuItemText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#000",
+    textAlign: "left"
+  }
 });
+export default UploadMediaScreen;
 
 const Button = (params) => {
   const btnStyle = {
-    backgroundColor: params.outline ? "#fff" : "#12D790",
+    backgroundColor: params.outline ? "#fff" : "#000",
     borderColor: params.outline ? "#000" : "#fff",
     borderWidth: 1
   };
@@ -85,13 +69,13 @@ const Button = (params) => {
 
 const buttonStyles = StyleSheet.create({
   btnContainer: {
-    paddingHorizontal: 40,
+    paddingTop: 10,
+    paddingHorizontal: 20,
     justifyContent: "center",
-    marginBottom: 10,
-    marginTop: 60
+    marginTop: 20
   },
   btn: {
-    backgroundColor: "#12D790",
+    backgroundColor: "black",
     height: 50,
     width: "100%",
     padding: 10,
@@ -113,4 +97,3 @@ const buttonStyles = StyleSheet.create({
     alignItems: "center"
   }
 });
-export default SubscriptionDetails;
